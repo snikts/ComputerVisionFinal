d1a432b1f306d95ba7324363b58c4858818d4e7c
Sarah <sarahjofficial@gmail.com>
2022-11-26 20:30:44 -0700

    added comments

--- M1Median.m
+++ M1Median.m
@@ -1,11 +1,18 @@
 function [centerVal] = M1Median(M1Neighborhood)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%M1Median Calculate pixel value for center pixel of neighborhood
+%   Partition M1Neighborhood into smaller neighborhoods. Calculate median
+%   of each neighborhood and find the mean of them all.
+
+% Find smaller neighborhood size
 [M1NeighborhoodRows, M1NeighborhoodCols] = size(M1Neighborhood);
 neighborhoodSize = sqrt(M1NeighborhoodRows);
-centerOffset = floor(neighborhoodSize/2);
+
+% Save the number of medians found and the sum of all the medians
 averageMedians = uint32(0);
 numMedians = uint32(0);
+
+% Loop through each neighborhood and find the median. Add it to sum of
+% medians.
 for r = 1:neighborhoodSize:M1NeighborhoodRows
     for c = 1:neighborhoodSize:M1NeighborhoodCols
         submatrix = M1Neighborhood(r:r+neighborhoodSize-1, c:c+neighborhoodSize-1);
@@ -14,5 +21,7 @@ for r = 1:neighborhoodSize:M1NeighborhoodRows
         numMedians = numMedians+1;
     end
 end
+
+% Calculate the mean of all medians in M1Neighborhood
 centerVal = averageMedians/numMedians;
 end
--- M2Median.m
+++ M2Median.m
@@ -1,7 +1,11 @@
 function [m2] = M2Median(M2Neighborhood)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%M2Median Calculate median of M2Neighborhood
+%   Places matrix into a list and calculates median of list
+
+% Place matrix into list
 m2n = M2Neighborhood';
 m2n = m2n(:)';
+
+% Calculate and return median
 m2 = median(m2n);
 end
